3dd5a753b73652a465b337fd824545408ed31afc
tine0328 <144678351+tine0328@users.noreply.github.com>
2023-09-14 15:57:56 +0800

    Add files via upload

---
+++ CalculatorApplication/CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    public delegate T Formula<T>(T arg1, T arg2);
+
+    internal class CalculatorClass
+    {
+        public Formula<String> formula;
+
+        public Double GetSum(double num1, double num2)
+        {
+            return (num1 + num2);
+        }
+
+        public Double GetDifference(double num1, double num2)
+        {
+            return (num1 - num2);
+        }
+        public Double GetProduct(double num1, double num2)
+        {
+            return (num1 * num2);
+        }
+        public Double GetQoutient(double num1, double num2)
+        {
+            return(num1 / num2);
+        }
+
+        public event Formula<double> CalculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Added the delegate " + value);
+            }
+            remove
+            {
+                Console.WriteLine("Removed the delegate " + value);
+            }
+        }
+    }
+}
---
+++ CalculatorApplication/CalculatorApplication/FrmCalculator.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CalculatorApplication
+{
+    partial class FrmCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtBoxInput1 = new TextBox();
+            txtBoxInput2 = new TextBox();
+            cbOperator = new ComboBox();
+            lblDisplayTotal = new Label();
+            btnEqual = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(175, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Enter the First Number: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(12, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(196, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Enter the Second Number: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(143, 214);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Answer:";
+            // 
+            // txtBoxInput1
+            // 
+            txtBoxInput1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtBoxInput1.Location = new Point(193, 38);
+            txtBoxInput1.Name = "txtBoxInput1";
+            txtBoxInput1.Size = new Size(248, 29);
+            txtBoxInput1.TabIndex = 3;
+            // 
+            // txtBoxInput2
+            // 
+            txtBoxInput2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtBoxInput2.Location = new Point(214, 128);
+            txtBoxInput2.Name = "txtBoxInput2";
+            txtBoxInput2.Size = new Size(227, 29);
+            txtBoxInput2.TabIndex = 4;
+            // 
+            // cbOperator
+            // 
+            cbOperator.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            cbOperator.FormattingEnabled = true;
+            cbOperator.Items.AddRange(new object[] { "+", "-", "*", "/" });
+            cbOperator.Location = new Point(259, 85);
+            cbOperator.Name = "cbOperator";
+            cbOperator.Size = new Size(121, 27);
+            cbOperator.TabIndex = 5;
+            // 
+            // lblDisplayTotal
+            // 
+            lblDisplayTotal.Font = new Font("Segoe UI", 12F, FontStyle.Underline, GraphicsUnit.Point);
+            lblDisplayTotal.Location = new Point(214, 214);
+            lblDisplayTotal.Name = "lblDisplayTotal";
+            lblDisplayTotal.Size = new Size(227, 21);
+            lblDisplayTotal.TabIndex = 6;
+            lblDisplayTotal.Text = "Answer:";
+            // 
+            // btnEqual
+            // 
+            btnEqual.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEqual.Location = new Point(250, 273);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new Size(139, 34);
+            btnEqual.TabIndex = 7;
+            btnEqual.Text = "Calculate";
+            btnEqual.UseVisualStyleBackColor = true;
+            btnEqual.Click += btnEqual_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(461, 331);
+            Controls.Add(btnEqual);
+            Controls.Add(lblDisplayTotal);
+            Controls.Add(cbOperator);
+            Controls.Add(txtBoxInput2);
+            Controls.Add(txtBoxInput1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtBoxInput1;
+        private TextBox txtBoxInput2;
+        private ComboBox cbOperator;
+        private Label lblDisplayTotal;
+        private Button btnEqual;
+    }
+}
